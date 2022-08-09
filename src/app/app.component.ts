7ee859fd9cf9df1fb5e9e42536e0d9c636e70860
januszpolak <janusz.polak@interia.pl>
2022-08-09 14:25:50 +0200

    add comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Angular-Counter';
 
+  // set counter to 0
   counterNumber = 0;
 
+  // reset counter
   reset(){
     this.counterNumber = 0;
   }
 
+  // increse counter add 1
   increse(){
     this.counterNumber = this.counterNumber + 1;
   }
 
+  // decrese counter subtract 1
   decrese(){
     this.counterNumber = this.counterNumber - 1;
   }
